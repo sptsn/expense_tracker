e26e23ef5e52d21ecd48012c27a7de5fae92e605
Alexander Spitsyn <sanspits@yandex.ru>
2019-09-12 18:54:13 +0300

    add Ledger class

--- spec/unit/app/api_spec.rb
+++ spec/unit/app/api_spec.rb
@@ -7,20 +7,75 @@
 # Visit http://www.pragmaticprogrammer.com/titles/rspec3 for more book information.
 #---
 require_relative '../../../app/api'
+require 'rack/test'
 
 module ExpenseTracker
   RSpec.describe API do
+    include Rack::Test::Methods
+
+    def app
+      API.new(ledger: ledger)
+    end
+
+    let(:ledger) { instance_double('ExpenseTracker::Ledger') }
+
     describe 'POST /expenses' do
       context 'when the expense is successfully recorded' do
-        it 'returns the expense id'
-        it 'responds with a 200 (OK)'
+        let(:expense) { { 'some' => 'data'} }
+        before do
+          allow(ledger).to receive(:record)
+            .with(expense)
+            .and_return(RecordResult.new(true, 417, nil))
+        end
+
+        it 'returns the expense id' do
+          post '/expenses', JSON.generate(expense)
+
+          parsed = JSON.parse(last_response.body)
+          expect(parsed).to include('expense_id' => 417)
+        end
+
+        it 'responds with a 200 (OK)' do
+          post '/expenses', JSON.generate(expense)
+          expect(last_response.status).to eq(200)
+        end
       end
 
-      # ... next context will go here...
-
       context 'when the expense fails validation' do
-        it 'returns an error message'
-        it 'responds with a 422 (Unprocessable entity)'
+        let(:expense) { { 'some' => 'data' } }
+
+        before do
+          allow(ledger).to receive(:record)
+            .with(expense)
+            .and_return(RecordResult.new(false, 417, 'Expense incomplete'))
+        end
+
+        it 'returns an error message' do
+          post '/expenses', JSON.generate(expense)
+
+          parsed = JSON.parse(last_response.body)
+          expect(parsed).to include('error' => 'Expense incomplete')
+        end
+
+        it 'responds with a 422 (Unprocessable entity)' do
+          post '/expenses', JSON.generate(expense)
+          expect(last_response.status).to eq(422)
+        end
+      end
+    end
+
+    describe 'GET /expenses/:date' do
+      before { get '/expenses/2019-09-11' }
+
+      context 'when expenses exist on the given date' do
+        it 'returns the expense records as JSON' do
+          parsed = JSON.parse(last_response.body)
+          expect(parsed).to_not be_nil
+        end
+
+        it 'responds with a 200 OK' do
+          expect(last_response.status).to eq 200
+        end
       end
     end
   end
